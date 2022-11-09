d9649f780e4dca088a6289ed8b13723f3a1ba784
Aleksandr677 <hristicenkoaleksandr@MacBook-Air-Hristicenko.local>
2022-11-09 18:36:23 +0300

    feat: add tabs

---
+++ Avocados/Avocados/View/AppView.swift
@@ -0,0 +1,46 @@
+//
+//  AppView.swift
+//  Avocados
+//
+//  Created by Христиченко Александр on 2022-11-09.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        TabView {
+            AvocadosView()
+                .tabItem {
+                    Image("tabicon-branch")
+                    Text("Avocados")
+                }
+            
+            ContentView()
+                .tabItem {
+                    Image("tabicon-book")
+                    Text("Recipes")
+                }
+            
+            RipeningStagesView()
+                .tabItem {
+                    Image("tabicon-avocado")
+                    Text("Ripening")
+                }
+            
+            SettingsView()
+                .tabItem {
+                    Image("tabicon-settings")
+                    Text("Settings")
+                }
+        } //TabView
+        .edgesIgnoringSafeArea(.top)
+        .accentColor(Color.primary)
+    }
+}
+
+struct AppView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppView()
+    }
+}
---
+++ Avocados/Avocados/View/AvocadosView.swift
@@ -0,0 +1,20 @@
+//
+//  AvocadosView.swift
+//  Avocados
+//
+//  Created by Христиченко Александр on 2022-11-09.
+//
+
+import SwiftUI
+
+struct AvocadosView: View {
+    var body: some View {
+        Text("Avocados")
+    }
+}
+
+struct AvocadosView_Previews: PreviewProvider {
+    static var previews: some View {
+        AvocadosView()
+    }
+}
