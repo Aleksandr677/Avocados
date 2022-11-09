a1f3ff36f7244905b7510e29531ec09e170fed5f
Aleksandr677 <hristicenkoaleksandr@MacBook-Air-Hristicenko.local>
2022-11-09 17:41:32 +0300

    feat: add Assets

---
+++ Avocados/Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Христиченко Александр on 2022-11-09.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
